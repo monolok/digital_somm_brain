ce86c8b9545599ed6ab5748ca7891d9768bd0bc5
monolok <antoinebertin@me.com>
2017-04-27 00:25:35 +0200

    fixing test controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,57 +49,72 @@ $("#show_wines_div").hide();
 		var body = document.getElementById("body").value;
 
 //palate_flavors data
-		var palate_flavors = [];
+		var palate_flavors = {};
+		var palate_flavors_fruit = [];
+		var palate_flavors_fruit_character = [];
+		var palate_flavors_non_fruit = [];
+		var palate_flavors_organic_earth = [];
+		var palate_flavors_inorganic_earth = [];
+		var palate_flavors_wood = [];
+		var palate_flavors_key = [];
 
-		if (document.getElementById("citrus_exist").checked) { palate_flavors.push("citrus")};
-		if (document.getElementById("apple_pear_exist").checked) { palate_flavors.push("apple/Pear")};
-		if (document.getElementById("stone_pit_exist").checked) { palate_flavors.push("stone/Pit")};
-		if (document.getElementById("tropical_exist").checked) { palate_flavors.push("tropical")};
-		if (document.getElementById("melon_exist").checked) { palate_flavors.push("melon")};
+		if (document.getElementById("citrus_exist").checked) { palate_flavors_fruit.push("citrus")};
+		if (document.getElementById("apple_pear_exist").checked) { palate_flavors_fruit.push("apple/Pear")};
+		if (document.getElementById("stone_pit_exist").checked) { palate_flavors_fruit.push("stone/Pit")};
+		if (document.getElementById("tropical_exist").checked) { palate_flavors_fruit.push("tropical")};
+		if (document.getElementById("melon_exist").checked) { palate_flavors_fruit.push("melon")};
 		
-		if (document.getElementById("red_exist").checked) { palate_flavors.push("red")};
-		if (document.getElementById("black_exist").checked) { palate_flavors.push("black")};
-		if (document.getElementById("blue_exist").checked) { palate_flavors.push("blue")};
+		if (document.getElementById("red_exist").checked) { palate_flavors_fruit.push("red")};
+		if (document.getElementById("black_exist").checked) { palate_flavors_fruit.push("black")};
+		if (document.getElementById("blue_exist").checked) { palate_flavors_fruit.push("blue")};
+		palate_flavors["fruit"] = palate_flavors_fruit;
+
+		if (document.getElementById("baked_exist").checked) { palate_flavors_fruit_character.push("baked")};
+		if (document.getElementById("stewed_exist").checked) { palate_flavors_fruit_character.push("stewed")};
+		if (document.getElementById("dried_exist").checked) { palate_flavors_fruit_character.push("dried")};
+		if (document.getElementById("peels_exist").checked) { palate_flavors_fruit_character.push("peels")};
+		if (document.getElementById("desiccated_exist").checked) { palate_flavors_fruit_character.push("desiccated")};
+		if (document.getElementById("tart_exist").checked) { palate_flavors_fruit_character.push("tart")};
+		if (document.getElementById("jammy_exist").checked) { palate_flavors_fruit_character.push("jammy")};
+		palate_flavors["fruit_character"] = palate_flavors_fruit_character;
+
+		if (document.getElementById("floral_exist").checked) { palate_flavors_non_fruit.push("floral")};
+		if (document.getElementById("vegetal_exist").checked) { palate_flavors_non_fruit.push("vegetal")};
+		if (document.getElementById("herbal_exist").checked) { palate_flavors_non_fruit.push("herbal")};
+		if (document.getElementById("spice_exist").checked) { palate_flavors_non_fruit.push("spice")};
+		if (document.getElementById("petrol_exist").checked) { palate_flavors_non_fruit.push("petrol")};
+		if (document.getElementById("animal_exist").checked) { palate_flavors_non_fruit.push("animal")};
+		if (document.getElementById("barn_exist").checked) { palate_flavors_non_fruit.push("barn")};
+		if (document.getElementById("fermentation_exist").checked) { palate_flavors_non_fruit.push("fermentation")};
+		palate_flavors["non_fruit"] = palate_flavors_non_fruit;
 		
-		if (document.getElementById("baked_exist").checked) { palate_flavors.push("baked")};
-		if (document.getElementById("stewed_exist").checked) { palate_flavors.push("stewed")};
-		if (document.getElementById("dried_exist").checked) { palate_flavors.push("dried")};
-		if (document.getElementById("peels_exist").checked) { palate_flavors.push("peels")};
-		if (document.getElementById("desiccated_exist").checked) { palate_flavors.push("desiccated")};
-		if (document.getElementById("tart_exist").checked) { palate_flavors.push("tart")};
-		if (document.getElementById("jammy_exist").checked) { palate_flavors.push("jammy")};
+		if (document.getElementById("forest_floor_exist").checked) { palate_flavors_organic_earth.push("forest Floor")};
+		if (document.getElementById("compost_exist").checked) { palate_flavors_organic_earth.push("compost")};
+		if (document.getElementById("mushrooms_exist").checked) { palate_flavors_organic_earth.push("mushrooms")};
+		if (document.getElementById("potting_soil_exist").checked) { palate_flavors_organic_earth.push("potting Soil")};
+		palate_flavors["organic_earth"] = palate_flavors_organic_earth;
 
-		if (document.getElementById("floral_exist").checked) { palate_flavors.push("floral")};
-		if (document.getElementById("vegetal_exist").checked) { palate_flavors.push("vegetal")};
-		if (document.getElementById("herbal_exist").checked) { palate_flavors.push("herbal")};
-		if (document.getElementById("spice_exist").checked) { palate_flavors.push("spice")};
-		if (document.getElementById("petrol_exist").checked) { palate_flavors.push("petrol")};
-		if (document.getElementById("animal_exist").checked) { palate_flavors.push("animal")};
-		if (document.getElementById("barn_exist").checked) { palate_flavors.push("barn")};
-		if (document.getElementById("fermentation_exist").checked) { palate_flavors.push("fermentation")};
+		if (document.getElementById("mineral_exist").checked) { palate_flavors_inorganic_earth.push("mineral")};
+		if (document.getElementById("wet_rock_exist").checked) { palate_flavors_inorganic_earth.push("wet Rock")};
+		if (document.getElementById("limestone_exist").checked) { palate_flavors_inorganic_earth.push("limestone")};
+		if (document.getElementById("chalk_exist").checked) { palate_flavors_inorganic_earth.push("chalk")};
+		if (document.getElementById("slate_exist").checked) { palate_flavors_inorganic_earth.push("slate")};
+		if (document.getElementById("flint_exist").checked) { palate_flavors_inorganic_earth.push("flint")};
+		palate_flavors["inorganic_earth"] = palate_flavors_inorganic_earth;
 
-		if (document.getElementById("forest_floor_exist").checked) { palate_flavors.push("forest Floor")};
-		if (document.getElementById("compost_exist").checked) { palate_flavors.push("compost")};
-		if (document.getElementById("mushrooms_exist").checked) { palate_flavors.push("mushrooms")};
-		if (document.getElementById("potting_soil_exist").checked) { palate_flavors.push("potting Soil")};
+		if (document.getElementById("old_exist").checked) { palate_flavors_wood.push("old")};
+		if (document.getElementById("new_exist").checked) { palate_flavors_wood.push("new")};
+		if (document.getElementById("large_exist").checked) { palate_flavors_wood.push("large")};
+		if (document.getElementById("small_exist").checked) { palate_flavors_wood.push("small")};
+		if (document.getElementById("french_exist").checked) { palate_flavors_wood.push("french")};
+		if (document.getElementById("american_exist").checked) { palate_flavors_wood.push("american")};
+		if (document.getElementById("no_wood_exist").checked) { palate_flavors_wood.push("no wood")};
+		palate_flavors["wood"] = palate_flavors_wood;
 
-		if (document.getElementById("mineral_exist").checked) { palate_flavors.push("mineral")};
-		if (document.getElementById("wet_rock_exist").checked) { palate_flavors.push("wet Rock")};
-		if (document.getElementById("limestone_exist").checked) { palate_flavors.push("limestone")};
-		if (document.getElementById("chalk_exist").checked) { palate_flavors.push("chalk")};
-		if (document.getElementById("slate_exist").checked) { palate_flavors.push("slate")};
-		if (document.getElementById("flint_exist").checked) { palate_flavors.push("flint")};
-
-		if (document.getElementById("old_exist").checked) { palate_flavors.push("old")};
-		if (document.getElementById("new_exist").checked) { palate_flavors.push("new")};
-		if (document.getElementById("large_exist").checked) { palate_flavors.push("large")};
-		if (document.getElementById("small_exist").checked) { palate_flavors.push("small")};
-		if (document.getElementById("french_exist").checked) { palate_flavors.push("french")};
-		if (document.getElementById("american_exist").checked) { palate_flavors.push("american")};
-		if (document.getElementById("no_wood_exist").checked) { palate_flavors.push("no wood")};
 		//Key element for testable flavor
 		var key_marker = document.getElementById("key_marker").value
-		if (!key_marker == false) { palate_flavors.push(key_marker)};
+		if (!key_marker == false) { palate_flavors_key.push(key_marker)};
+		palate_flavors["key"] = palate_flavors_key;
 
 //AJAX
 		$.ajax({
--- app/controllers/testables_controller.rb
+++ app/controllers/testables_controller.rb
@@ -12,44 +12,58 @@ class TestablesController < ApplicationController
 		while i < @grapes.count
 			z = 0
 			while z < @grapes[i].palate_structures.count
-				#STRUCURE: structure is recorded in ranges so no need to test different options
+				#STRUCURE (structure is recorded in ranges so no need to test different options)
 				@if_tannin_true = @grapes[i].palate_structures[z].tannin.include?(params["tannin"].to_i)
 				@if_acid_true = @grapes[i].palate_structures[z].acid.include?(params["acid"].to_i)
 				@if_alcohol_true = @grapes[i].palate_structures[z].alcohol.include?(params["alcohol"].to_i)
 				@if_body_true = @grapes[i].palate_structures[z].body.include?(params["body"].to_i)
 
-				#FLAVOR: check how many flavors checked are similar with @grapes[i].palate_flavors[z] in database to define true
-				a = 0
+				#FLAVOR
 				if not params["palate_flavors"].nil?
-					while a < params["palate_flavors"].count
-						if @grapes[i].palate_flavors[z].fruit.include?(params["palate_flavors"][a])
+
+					if params["palate_flavors"]["fruit"].nil? == false
+						f = @grapes[i].palate_flavors[z].fruit & params["palate_flavors"]["fruit"]
+						if not f.empty?
 							@if_fruit_true = true
-
-							#FIGURE OUT what happen if 3 true and 2 false ?  
-							#for 1 true and any false @if_fruit_true will remain TRUE, it is a PB
-
-						elsif true#@grapes[i].palate_flavors[z].fruit_character.include?(params["palate_flavors"][a])
-							@if_fruit_character_true = true
-						elsif true#@grapes[i].palate_flavors[z].non_fruit.include?(params["palate_flavors"][a])
-							@if_non_fruit_true = true
-						elsif true#@grapes[i].palate_flavors[z].organic_earth.include?(params["palate_flavors"][a])
-							@if_organic_earth_true = true
-						elsif true#@grapes[i].palate_flavors[z].inorganic_earth.include?(params["palate_flavors"][a])
-							@if_inorganic_earth_true = true			
-						elsif true#@grapes[i].palate_flavors[z].wood.include?(params["palate_flavors"][a])
-							@if_wood_true = true
-						else
-							
 						end
-						a+=1
+						#FIGURE OUT what happen if 3 true and 2 false ?  
+						#for 1 true and any false @if_fruit_true will remain TRUE, it is a PB
+						# play with variable f which return array ONLY with checked box value that are coorect
+
+					elsif params["palate_flavors"]["fruit_character"].nil? == false
+						fc = @grapes[i].palate_flavors[z].fruit_character & params["palate_flavors"]["fruit_character"]
+						if not fc.empty?
+							@if_fruit_character_true = true
+						end
+
+					elsif params["palate_flavors"]["non_fruit"].nil? == false
+						nf = @grapes[i].palate_flavors[z].non_fruit & params["palate_flavors"]["non_fruit"]
+						if not nf.empty?
+							@if_non_fruit_true = true
+						end						
+					elsif params["palate_flavors"]["organic_earth"].nil? == false
+						oe = @grapes[i].palate_flavors[z].organic_earth & params["palate_flavors"]["organic_earth"]
+						if not nf.empty?
+							@if_organic_earth_true = true
+						end						
+					elsif params["palate_flavors"]["inorganic_earth"].nil? == false
+						ie = @grapes[i].palate_flavors[z].inorganic_earth & params["palate_flavors"]["inorganic_earth"]
+						if not nf.empty?
+							@if_inorganic_earth_true = true			
+						end
+					elsif params["palate_flavors"]["wood"].nil? == false
+						w = @grapes[i].palate_flavors[z].wood & params["palate_flavors"]["wood"]
+						if not nf.empty?
+							@if_wood_true = true
+						end
+					#elsif params["palate_flavors"]["key"].nil? == false
+						#FIGURE OUT WHAT TO DO WITH KEY MARKER
+					else
 					end
 				end
-				puts "grape #{@grapes[i].name} has fruit #{params["palate_flavors"]}? #{@if_fruit_true}"
-
-				#FIGURE OUT HOW TO INTEGRATE KEY_MARKER ELEMENT
-
-				#RESULT
-				if @if_tannin_true && @if_acid_true && @if_alcohol_true && @if_body_true && @if_fruit_true #&& @if_fruit_character_true && @if_non_fruit_true && @if_organic_earth_true && @if_inorganic_earth_true && @if_wood_true
+				
+				#RESULT: FIGURE OUT HOW TO RUN THIS, EVEN WHEN FRUIT, NON FRUIT... ARE NOT PRESENT
+				if @if_tannin_true && @if_acid_true && @if_alcohol_true && @if_body_true && @if_fruit_true #&& @if_fruit_character_true #&& @if_non_fruit_true && @if_organic_earth_true && @if_inorganic_earth_true && @if_wood_true
 					y = 0
 					while y < @grapes[i].testables.count
 						@result << @grapes[i].testables[y].name
