ab81131fec2439579d7f9adb21b484382035443e
monolok <antoinebertin@me.com>
2017-05-02 11:02:46 +0200

    seeding like a MF

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,10 @@
 cab = Grape.create(name: "Cabernet Sauvignon and blends")
 	#Graves
 		Testable.create(name: "Graves", grape_id: cab.id)
-		graves_structure = PalateStructure.create(body: (4..5), alcohol: (4..5), acid: (4..4), tannin: (4..5))
-		graves_flavor = PalateFlavor.create(fruit: ["black", "berry", "cherry", "cassis", "green olive"], fruit_character: ["tart", "stewed"], non_fruit: ["herbal", "animal", "cedar", "pencil lead", "violet"], organic_earth: ["forest floor", "mushrooms", "spice"], inorganic_earth: [""], wood: ["new", "old", "french"])
-		graves_nose = Nose.create(intensity: ["delicate", "moderate"], fruit: ["black", "berry", "cherry", "cassis", "green olive"], fruit_character: ["tart", "stewed"], non_fruit: ["herbal", "animal", "cedar", "pencil lead", "violet"], organic_earth: ["forest floor", "mushrooms", "spice"], inorganic_earth: [""], wood: ["new", "old", "french"])
-		graves_sight = Sight.create(color: (2..2), concentration: (3..3))
-
-		cab.palate_structures << graves_structure
-		cab.palate_flavors << graves_flavor
-		cab.noses << graves_nose
-		cab.sights << graves_sight
+		cab.palate_structures << PalateStructure.create(body: (4..5), alcohol: (4..5), acid: (4..4), tannin: (4..5))
+		cab.palate_flavors << PalateFlavor.create(fruit: ["black", "berry", "cherry", "cassis", "green olive"], fruit_character: ["tart", "stewed"], non_fruit: ["herbal", "animal", "cedar", "pencil lead", "violet"], organic_earth: ["forest floor", "mushrooms", "spice"], inorganic_earth: [""], wood: ["new", "old", "french"])
+		cab.noses << Nose.create(intensity: ["delicate", "moderate"], fruit: ["black", "berry", "cherry", "cassis", "green olive"], fruit_character: ["tart", "stewed"], non_fruit: ["herbal", "animal", "cedar", "pencil lead", "violet"], organic_earth: ["forest floor", "mushrooms", "spice"], inorganic_earth: [""], wood: ["new", "old", "french"])
+		cab.sights << Sight.create(color: (2..2), concentration: (3..3))
 
 	#Medoc
 	#Barossa Valley
@@ -21,17 +16,14 @@ cab = Grape.create(name: "Cabernet Sauvignon and blends")
 	#Maipo
 	#Maule
 	#Rapel
+
 	#California (Napa, Sonoma, Central coast)
 		Testable.create(name: "California Cab", grape_id: cab.id)
-		cali_cab_structure = PalateStructure.create(body: (4..5), alcohol: (4..5), acid: (3..4), tannin: (4..5))
-		cali_cab_flavor = PalateFlavor.create(fruit: ["black", "berry", "cherry", "cassis", "currant", "green olive"], fruit_character: ["jammy", "stewed"], non_fruit: ["herbal", "spice", "chocolate", "cedar"], organic_earth: [""], inorganic_earth: [""], wood: ["new", "old", "french", "american"])
-		#cali_cab_nose = nil
-		#cali_cab_sight = nil
+		cab.palate_structures << PalateStructure.create(body: (4..5), alcohol: (4..5), acid: (3..4), tannin: (4..5))
+		cab.palate_flavors << PalateFlavor.create(fruit: ["black", "berry", "cherry", "cassis", "currant", "green olive"], fruit_character: ["jammy", "stewed"], non_fruit: ["herbal", "spice", "chocolate", "cedar"], organic_earth: [""], inorganic_earth: [""], wood: ["new", "old", "french", "american"])
+		cab.noses <<  Nose.create(intensity: ["moderate", "powerful"], fruit: ["black", "berry", "cherry", "cassis", "currant", "green olive"], fruit_character: ["jammy", "stewed"], non_fruit: ["herbal", "spice", "chocolate", "cedar"], organic_earth: [""], inorganic_earth: [""], wood: ["new", "old", "french", "american"])
+		cab.sights << Sight.create(color: (2..2), concentration: (3..3))
 
-		cab.palate_structures << cali_cab_structure
-		cab.palate_flavors << cali_cab_flavor
-		#cab.noses << cali_cab_nose
-		#cab.sights << cali_cab_sight
 	#Washington (Columbia valley)
 
 #Cabernet Franc
@@ -39,15 +31,10 @@ cab_franc = Grape.create(name: "Cabernet Franc")
 	#Bourgueil
 	#Chinon
 		Testable.create(name: "Chinon", grape_id: cab_franc.id)
-		chinon_structure = PalateStructure.create(body: (3..3), alcohol: (3..4), acid: (4..5), tannin: (3..4))
-		chinon_flavor = PalateFlavor.create(fruit: ["red", "sour cherry", "plum", "raspberry", "cranberry"], fruit_character: ["tart"], non_fruit: ["vegetal", "herbal", "floral"], organic_earth: [""], inorganic_earth: ["mineral", "chalk", "wet rock", "limestone"], wood: ["no wood", "old", "new"])
-		#chinon_nose = nil
-		#chinon_sight = nil
-
-		cab_franc.palate_structures << chinon_structure
-		cab_franc.palate_flavors << chinon_flavor
-		#cab_franc.noses << chinon_nose
-		#cab_franc.sights << chinon_sight
+		cab_franc.palate_structures << PalateStructure.create(body: (3..3), alcohol: (3..4), acid: (4..5), tannin: (3..4))
+		cab_franc.palate_flavors << PalateFlavor.create(fruit: ["red", "sour cherry", "plum", "raspberry", "cranberry"], fruit_character: ["tart"], non_fruit: ["vegetal", "herbal", "floral"], organic_earth: [""], inorganic_earth: ["mineral", "chalk", "wet rock", "limestone"], wood: ["no wood", "old", "new"])
+		cab_franc.noses << Nose.create(intensity: ["moderate"], fruit: ["red", "sour cherry", "plum", "raspberry", "cranberry"], fruit_character: ["tart"], non_fruit: ["vegetal", "herbal", "floral"], organic_earth: [""], inorganic_earth: ["mineral", "chalk", "wet rock", "limestone"], wood: ["no wood", "old", "new"])
+		cab_franc.sights << Sight.create(color: (2..2), concentration: (2..3))
 
 	#St. Nicolas de Bourgueil
 
@@ -64,15 +51,11 @@ cab_franc = Grape.create(name: "Cabernet Franc")
 #Gamay
 gamay = Grape.create(name: "Gamay")
 Testable.create(name: "Beaujolais", grape_id: gamay.id)
-gamay_structure = PalateStructure.create(body: (2..4), alcohol: (3..4), acid: (4..4), tannin: (2..3))
-gamay_flavor = PalateFlavor.create(fruit: ["red", "artificial fruit", "tropical", "bubblegum"], fruit_character: ["tart", "baked", "candied"], non_fruit: ["herbal", "floral"], organic_earth: [""], inorganic_earth: ["mineral", "wet rock", "limestone", "slate"], wood: ["no wood"])
-#gamay_nose = nil
-#gamay_sight = nil
+gamay.palate_structures << PalateStructure.create(body: (2..4), alcohol: (3..4), acid: (4..4), tannin: (2..3))
+gamay.palate_flavors << PalateFlavor.create(fruit: ["red", "artificial fruit", "tropical", "bubblegum"], fruit_character: ["tart", "baked", "candied"], non_fruit: ["herbal", "floral"], organic_earth: [""], inorganic_earth: ["mineral", "wet rock", "limestone", "slate"], wood: ["no wood"])
+gamay.noses << Nose.create(intensity: ["moderate", "powerful"], fruit: ["red", "artificial fruit", "tropical", "bubblegum"], fruit_character: ["tart", "baked", "candied"], non_fruit: ["herbal", "floral"], organic_earth: [""], inorganic_earth: ["mineral", "wet rock", "limestone", "slate"], wood: ["no wood"])
+gamay.sights << Sight.create(color: (1..2), concentration: (2..3))
 
-gamay.palate_structures << gamay_structure
-gamay.palate_flavors << gamay_flavor
-#gamay.noses << gamay_nose
-#gamay.sights << gamay_sight
 	#Beaujolais (carbonic maceration)
 	#Beaujolais (no carbonic maceration)
 
@@ -80,15 +63,10 @@ gamay.palate_flavors << gamay_flavor
 gsm = Grape.create(name: "Grenache Blend")
 	#CDP
 	Testable.create(name: "CDP", grape_id: gsm.id)
-	cdp_structure = PalateStructure.create(body: (4..5), alcohol: (4..5), acid: (3..4), tannin: (4..5))
-	cdp_flavor = PalateFlavor.create(fruit: ["black", "red"], fruit_character: ["baked", "ripe"], non_fruit: ["herbal", "garrigue", "spice", "pepper", "savory", "animal"], organic_earth: ["mushrooms", "forest floor", "potting soil"], inorganic_earth: ["limestone", "mineral", "wet rock", "slate", "flint"], wood: ["old", "french", "american", "large"])
-	#cdp_nose = nil
-	#cdp_sight = nil
-
-	gsm.palate_structures << cdp_structure
-	gsm.palate_flavors << cdp_flavor
-	#gsm.noses << cdp_nose
-	#gsm.sights << cdp_sight
+	gsm.palate_structures << PalateStructure.create(body: (4..5), alcohol: (4..5), acid: (3..4), tannin: (4..5))
+	gsm.palate_flavors << PalateFlavor.create(fruit: ["black", "red"], fruit_character: ["baked", "ripe"], non_fruit: ["herbal", "garrigue", "spice", "pepper", "savory", "animal"], organic_earth: ["mushrooms", "forest floor", "potting soil"], inorganic_earth: ["limestone", "mineral", "wet rock", "slate", "flint"], wood: ["old", "french", "american", "large"])
+	gsm.noses << Nose.create(intensity: ["moderate", "powerful"], fruit: ["black", "red"], fruit_character: ["baked", "ripe"], non_fruit: ["herbal", "garrigue", "spice", "pepper", "savory", "animal"], organic_earth: ["mushrooms", "forest floor", "potting soil"], inorganic_earth: ["limestone", "mineral", "wet rock", "slate", "flint"], wood: ["old", "french", "american", "large"])
+	gsm.sights << Sight.create(color: (2..2), concentration: (2..3))
 
 	#Gigondas
 	#Vacqueyras
@@ -107,11 +85,10 @@ gsm = Grape.create(name: "Grenache Blend")
 nebbiolo = Grape.create(name: "Nebbiolo")
 	#Barolo
 		Testable.create(name: "Barolo", grape_id: nebbiolo.id)
-		barolo_structure = PalateStructure.create(body: (4..5), alcohol: (4..5), acid: (4..5), tannin: (4..5))
-		barolo_flavor = PalateFlavor.create(fruit: ["red", "cherry", "cranberry"], fruit_character: ["tart", "dried"], non_fruit: ["floral", "vegetal", "truffle", "rose petal"], organic_earth: ["forest floor", "mushrooms", "potting soil"], inorganic_earth: ["mineral"], wood: ["new", "old", "french", "american"])
-
-		nebbiolo.palate_structures << barolo_structure
-		nebbiolo.palate_flavors << barolo_flavor
+		nebbiolo.palate_structures << PalateStructure.create(body: (4..5), alcohol: (4..5), acid: (4..5), tannin: (4..5))
+		nebbiolo.palate_flavors << PalateFlavor.create(fruit: ["red", "cherry", "cranberry"], fruit_character: ["tart", "dried"], non_fruit: ["floral", "vegetal", "truffle", "rose petal"], organic_earth: ["forest floor", "mushrooms", "potting soil"], inorganic_earth: ["mineral"], wood: ["new", "old", "french", "american"])
+		nebbiolo.noses << Nose.create(intensity: ["moderate", "powerful"], fruit: ["red", "cherry", "cranberry"], fruit_character: ["tart", "dried"], non_fruit: ["floral", "vegetal", "truffle", "rose petal"], organic_earth: ["forest floor", "mushrooms", "potting soil"], inorganic_earth: ["mineral"], wood: ["new", "old", "french", "american"])
+		nebbiolo.sights << Sight.create(color: (2..4), concentration: (2..3), rim: true) #strong rim variation
 
 	#Barbaresco
 	#Gattinara (normale & riserva)
@@ -120,11 +97,10 @@ nebbiolo = Grape.create(name: "Nebbiolo")
 pn = Grape.create(name: "Pinot Noir")
 	#Côte d'Or
 		Testable.create(name: "Côte d'Or", grape_id: pn.id)
-		cote_dor_structure = PalateStructure.create(body: (2..3), alcohol: (2..4), acid: (4..5), tannin: (2..3))
-		cote_dor_flavor = PalateFlavor.create(fruit: ["red", "cherry", "raspberry", "strawberry"], fruit_character: ["tart"], non_fruit: ["floral", "herbal", "spice", "vegetal", "tea"], organic_earth: ["forest floor", "potting soil"], inorganic_earth: ["mineral", "limestone", "wet rock", "slate", "flint"], wood: ["new", "old", "french", "american"])
-		
-		pn.palate_structures << cote_dor_structure
-		pn.palate_flavors << cote_dor_flavor
+		pn.palate_structures << PalateStructure.create(body: (2..3), alcohol: (2..4), acid: (4..5), tannin: (2..3))
+		pn.palate_flavors << PalateFlavor.create(fruit: ["red", "cherry", "raspberry", "strawberry"], fruit_character: ["tart"], non_fruit: ["floral", "herbal", "spice", "vegetal", "tea"], organic_earth: ["forest floor", "potting soil"], inorganic_earth: ["mineral", "limestone", "wet rock", "slate", "flint"], wood: ["new", "old", "french", "american"])
+		pn.noses << Nose.create(intensity: ["delicate", "moderate"], fruit: ["red", "cherry", "raspberry", "strawberry"], fruit_character: ["tart"], non_fruit: ["floral", "herbal", "spice", "vegetal", "tea"], organic_earth: ["forest floor", "potting soil"], inorganic_earth: ["mineral", "limestone", "wet rock", "slate", "flint"], wood: ["new", "old", "french", "american"])
+		pn.sights << Sight.create(color: (2..2), concentration: (1..2))
 
 	#California (Anderson valley, Carneros, Central coast, Sonoma coast/valley)
 	#Oregon (Willamette valley)
@@ -133,23 +109,25 @@ pn = Grape.create(name: "Pinot Noir")
 	#Martinborough
 
 #Sangiovese
+sangiovese = Grape.create(name: "Sangiovese")
 	#Brunello di Montalcino
 	#Chianti
+	Testable.create(name: "Chianti", grape_id: sangiovese.id)
+	sangiovese.palate_structures << PalateStructure.create(body: (3..4), alcohol: (3..4), acid: (4..5), tannin: (4..5))
+	sangiovese.palate_flavors << PalateFlavor.create(fruit: ["red", "tomato", "cherry"], fruit_character: ["tart", "sharp", "bright"], non_fruit: ["floral", "herbal", "vegetal", "leafy", "sandalwood"], organic_earth: ["forest floor", "earth", "potting soil"], inorganic_earth: ["mineral", "chalk"], wood: ["old", "french", "american"])
+	sangiovese.noses << Nose.create(intensity: ["moderate", "powerful"], fruit: ["red", "tomato", "cherry"], fruit_character: ["tart", "sharp", "bright"], non_fruit: ["floral", "herbal", "vegetal", "leafy", "sandalwood"], organic_earth: ["forest floor", "earth", "potting soil"], inorganic_earth: ["mineral", "chalk"], wood: ["old", "french", "american"])
+	sangiovese.sights << Sight.create(color: (2..2), concentration: (2..3))
+
 	#vino Nobile di Montepulciano (classico, riserva, ruffina)
 
 #Syrah
 syrah = Grape.create(name: "Syrah")
 	#Norhtern Rhône
 	Testable.create(name: "Norhtern Rhône", grape_id: syrah.id)
-	nr_structure = PalateStructure.create(body: (4..5), alcohol: (3..4), acid: (3..4), tannin: (4..5))
-	nr_flavor = PalateFlavor.create(fruit: ["black", "red", "green", "plum"], fruit_character: ["tart"], non_fruit: ["herbal", "spice", "floral", "animal", "pepper", "provence herbs", "tobacco", "ash", "smoky-meat", "meat"], organic_earth: ["mushrooms", "forest floor"], inorganic_earth: ["mineral", "limestone", "wet rock"], wood: ["old", "french", "american", "large"])
-	#nr_nose = nil
-	#nr_sight = nil
-
-	syrah.palate_structures << nr_structure
-	syrah.palate_flavors << nr_flavor
-	#syrah.noses << nr_nose
-	#syrah.sights << nr_sight
+	syrah.palate_structures << PalateStructure.create(body: (4..5), alcohol: (3..4), acid: (3..4), tannin: (4..5))
+	syrah.palate_flavors << PalateFlavor.create(fruit: ["black", "red", "green", "plum"], fruit_character: ["tart"], non_fruit: ["herbal", "spice", "floral", "animal", "pepper", "provence herbs", "tobacco", "ash", "smoky-meat", "meat"], organic_earth: ["mushrooms", "forest floor"], inorganic_earth: ["mineral", "limestone", "wet rock"], wood: ["old", "french", "american", "large"])
+	syrah.noses << Nose.create(intensity: ["moderate", "powerful"], fruit: ["black", "red", "green", "plum"], fruit_character: ["tart"], non_fruit: ["herbal", "spice", "floral", "animal", "pepper", "provence herbs", "tobacco", "ash", "smoky-meat", "meat"], organic_earth: ["mushrooms", "forest floor"], inorganic_earth: ["mineral", "limestone", "wet rock"], wood: ["old", "french", "american", "large"])
+	syrah.sights << Sight.create(color: (1..2), concentration: (1..2))
 
 	#South Australia
 	#Victoria
